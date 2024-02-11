acc2a3f6c90424be4b7a389edd0bdccc38be69cb
kyori19 <kyori@accelf.net>
2024-02-11 06:45:35 +0000

    move export-pdf

---
+++ src/export-pdf.ts
@@ -0,0 +1,63 @@
+import { JupyterFrontEnd } from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import { NotebookPanel } from '@jupyterlab/notebook';
+import { IRenderMimeRegistry, MimeModel } from '@jupyterlab/rendermime';
+import { KernelMessage } from '@jupyterlab/services';
+
+export const export_pdf = async (app : JupyterFrontEnd, rendermime : IRenderMimeRegistry) => {
+  const nb = app.shell.currentWidget;
+  if (!(nb instanceof NotebookPanel) || !nb.model) {
+    return;
+  }
+  const workingKernel = nb.sessionContext.session?.kernel;
+  if (!workingKernel) {
+    return;
+  }
+
+  const cells = nb.model.sharedModel.cells.filter(
+    ({ cell_type }) => cell_type === 'code'
+  );
+  const mods = cells
+    .filter(({ source }) => source.startsWith('%!'))
+    .map(({ source }) => source.slice(2));
+  const document = cells
+    .filter(
+      ({ source }) =>
+        !source.startsWith('%!') &&
+        !source.startsWith('%?') &&
+        !source.startsWith('%%')
+    )
+    .map(({ source }) => source)
+    .join('\n');
+
+  const renderingKernel = await app.serviceManager.kernels.startNew(
+    {
+      name: workingKernel.name
+    },
+    {
+      clientId: workingKernel.clientId,
+      username: workingKernel.username,
+      handleComms: workingKernel.handleComms
+    }
+  );
+  renderingKernel.requestExecute({ code: '%% render-in-pdf' });
+  mods.forEach(mod => renderingKernel.requestExecute({ code: mod }));
+  const future = renderingKernel.requestExecute({ code: document });
+  future.onIOPub = msg => {
+    if (!KernelMessage.isExecuteResultMsg(msg)) {
+      return;
+    }
+
+    renderingKernel.shutdown();
+    const pdf = rendermime.createRenderer('application/pdf');
+    const tab = new MainAreaWidget({
+      content: pdf
+    });
+    tab.title.label = `PDF Export: ${nb.title.label}`;
+    tab.id = `${nb.id}-exported-pdf`;
+    tab.revealed.then(() => {
+      pdf.renderModel(new MimeModel(msg.content));
+    });
+    app.shell.add(tab, 'main', { mode: 'tab-after' });
+  };
+};
